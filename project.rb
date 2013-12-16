eb57a7b54c1fb08241b6af3c9af05728e177ff1a
Aaron McLeod <aaron.mcleod@mosaic.com>
2013-12-16 14:14:58 -0500

    added description to ggj entry

--- project.rb
+++ project.rb
@@ -4,7 +4,7 @@ class Project
   class << self
     def all
       [
-        {name: 'Global Game Jam 2013', url: '/ggj2013'},
+        {name: 'Global Game Jam 2013', url: '/ggj2013', description: "Top down action game that I made with a few others for the global game jam in 2013."},
         {name: 'January Knight', url: '/january-knight.zip', description: 'Unzip the file, and run via command line', instructions: 'java -jar january-knight.jar'},
         {name: 'Badass Stickman', url: '/badass-stickman.zip', description: 'Unzip the file, and run via command line', instructions: 'java -jar badass-stickman.jar'},
         {name: 'LudumDare 26', url: '/ld26.zip', description: 'Unzip the file, and run via command line', instructions: 'java -jar ld26.jar'},
