5c771917fbe5bea9bec2efde08b446c61421884d
Aaron McLeod <aaron.mcleod@mosaic.com>
2014-05-14 17:28:04 -0400

    added ignore statement for data file. some minor refactoring

--- rakefile.rb
+++ rakefile.rb
@@ -5,8 +5,11 @@ require_relative 'project'
 desc 'builds static site'
 task :build do
   FileUtils.mkdir_p('build')
+
+  layout_file = File.read("sitefiles/layouts/layout.haml")
+
   Dir['sitefiles/*.haml'].each do |file|
-    rendered_html = Haml::Engine.new(File.read("sitefiles/layouts/layout.haml")).render do
+    rendered_html = Haml::Engine.new(layout_file).render do
       Haml::Engine.new(File.read(file)).render
     end
     File.open("build/#{file.gsub('sitefiles/', '').gsub('haml','html')}", 'w+') do |f|
