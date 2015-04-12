c0b15de4609d162786c1c5d804f9cd3147fc125d
Aaron McLeod <sircoolguy99@gmail.com>
2015-04-12 17:32:24 -0400

    removed full project page stuff

--- rakefile.rb
+++ rakefile.rb
@@ -17,20 +17,6 @@ task :build do
     f.write rendered_html
   end
 
-  Project.all.each do |project|
-    if project.full_page?
-      rendered_html = Haml::Engine.new(layout_file).render do
-        Haml::Engine.new(File.read('sitefiles/project.haml')).render project
-      end
-
-      FileUtils.mkdir_p "build/#{project.slug}"
-
-      File.open("build/#{project.slug}/index.html", "w+") do |f|
-        f.write rendered_html
-      end
-    end
-  end
-
   FileUtils.cp_r('sitefiles/images', 'build')
   FileUtils.cp_r('sitefiles/javascripts', 'build')
   FileUtils.cp_r('sitefiles/stylesheets', 'build')
